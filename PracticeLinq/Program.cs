380f6630493f91fc4525a0e7662255918c32061d
elimarvin99 <elimarvin99@gmail.com>
2021-11-12 12:51:04 -0500

    finished exercise

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>() { "Fortnite", "Call of Duty", "Far Cry", "Battlefield", "Madden", "Assasins Creed" };
+            var orderdVideoGames = videoGames.OrderBy(x => x.Length).ToList();       //need to add to list because otherwise the variable is returned as a IEnumerable type
+            orderdVideoGames.ForEach(x => Console.WriteLine(x));
+
+            //foreach (var item in orderdVideoGames)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
